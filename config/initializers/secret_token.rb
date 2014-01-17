af7feed62aa5459bfa8a4c7458b21249b8a215ab
Andreas Fornell <nelly@lindenlab.com>
2014-01-17 11:37:45 -0800

    Mockups

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+U2udemo::Application.config.secret_token = 'fcfc122a688589e4ee1670b0ac917713d3d530386de1cfb105f79ef4e254a4300dfe85b3180986ac51a79cc33f9cca81400324a75421d1f5d81fe900b73f71a5'
